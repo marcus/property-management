25e5feb6951d292c889bfd40c9c0a9b2e45a79fb
Marcus Vorwaller <marcus@vorwaller.net>
2008-06-21 15:33:26 -0700

    Minor change to test cron

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,3 @@
-# Be sure to restart your server when you modify this file
 
 # Uncomment below to force Rails into production mode when
 # you don't control web/app server and can't set it the proper way

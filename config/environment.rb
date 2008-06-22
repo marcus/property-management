6424daaba2680572e258a6c5b282ae206f20b3a1
Marcus Vorwaller <marcus@vorwaller.net>
2008-06-21 18:12:12 -0700

    Test 4

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 
-# Uncomment  below to force Rails into production mode when
+# Uncomment  below  to force Rails into production mode when
 # you don't control web/app server and can't set it the proper way
 # ENV['RAILS_ENV'] ||= 'production'
 

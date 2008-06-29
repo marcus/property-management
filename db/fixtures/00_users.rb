14fdb9ad41287c11b82a69d571ea9e799bd92137
Marcus Vorwaller <marcus@vorwaller.net>
2008-06-28 23:44:21 -0700

    Seed fix

--- db/fixtures/00_users.rb
+++ db/fixtures/00_users.rb
@@ -2,6 +2,7 @@ User.seed(:login) do |s|
   s.login = "marcus"
   s.email = "marcus@vorwaller.net"
   s.password = "marcusmarcus"
+  s.password_confirmation = "marcusmarcus"
   s.status = 1
   s.firstname = "Marcus"
   s.lastname = "Vorwaller"

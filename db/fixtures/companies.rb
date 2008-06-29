4bf5fb81a5311ca6d7d351975568037f20dd7eed
Marcus Vorwaller <marcus@vorwaller.net>
2008-06-28 23:35:18 -0700

    Basic fixtures

---
+++ db/fixtures/companies.rb
@@ -0,0 +1,4 @@
+Company.seed(:name) do |s|
+  s.name = "Northwest Properties"
+  s.description = "This is the description"
+end
---
+++ db/fixtures/properties.rb
@@ -0,0 +1,11 @@
+Property.seed(:name) do |s|
+  s.name = "Jackson Hole"
+end
+
+Property.seed(:name) do |s|
+  s.name = "Whistler"
+end
+
+Property.seed(:name) do |s|
+  s.name = "Hawaii"
+end

a43a82c3149a3bf006dfe2528d4b478542efc225
Marcus Vorwaller <marcus@vorwaller.net>
2008-06-28 23:47:20 -0700

    Again

--- db/fixtures/02_properties.rb
+++ db/fixtures/02_properties.rb
@@ -1,14 +1,14 @@
 Property.seed(:name) do |s|
   s.name = "Jackson Hole"
-  s.company = Company.first.id
+  s.company = Company.first
 end
 
 Property.seed(:name) do |s|
   s.name = "Whistler"
-  s.company = Company.first.id
+  s.company = Company.first
 end
 
 Property.seed(:name) do |s|
   s.name = "Hawaii"
-  s.company = Company.first.id
+  s.company = Company.first
 end

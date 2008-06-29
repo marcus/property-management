682d194c0c227d704c48878e85200508e1e3f0ca
Marcus Vorwaller <marcus@vorwaller.net>
2008-06-28 23:41:54 -0700

    Moving around of the fixtures

---
+++ db/fixtures/00_users.rb
@@ -0,0 +1,8 @@
+User.seed(:login) do |s|
+  s.login = "marcus"
+  s.email = "marcus@vorwaller.net"
+  s.password = "marcusmarcus"
+  s.status = 1
+  s.firstname = "Marcus"
+  s.lastname = "Vorwaller"
+end

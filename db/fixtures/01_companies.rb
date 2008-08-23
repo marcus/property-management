b4042c4779f7c0b5756ed70d0ac6dab5607bc802
Marcus Vorwaller <marcus@vorwaller.net>
2008-08-23 15:32:27 -0700

    Company seed bugfix

--- db/fixtures/01_companies.rb
+++ db/fixtures/01_companies.rb
@@ -2,7 +2,5 @@ Company.seed(:name) do |s|
   s.name = "Northwest Properties"
   s.description = "Northwest Properties is an exclusive vacation rental property provider in Hawaii, Jackson Hole, Wyoming and BC, Canada."
   s.email = "carl@ckjohnson.com"
-  s.logo = "#{RAILS_ROOT}/simages/nwproperties_logo.jpg"
-  s.logo = File.new("#{RAILS_ROOT}/simages/jackson02.jpg")
-  
+  s.logo = File.new("#{RAILS_ROOT}/simages/nwproperties_logo.jpg")
 end

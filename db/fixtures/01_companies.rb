3ebf19ab06a0eeda408dddbf0945ecf50d668d23
Marcus Vorwaller <marcus@vorwaller.net>
2008-08-23 15:30:30 -0700

    Company logo fixture

--- db/fixtures/01_companies.rb
+++ db/fixtures/01_companies.rb
@@ -3,4 +3,6 @@ Company.seed(:name) do |s|
   s.description = "Northwest Properties is an exclusive vacation rental property provider in Hawaii, Jackson Hole, Wyoming and BC, Canada."
   s.email = "carl@ckjohnson.com"
   s.logo = "#{RAILS_ROOT}/simages/nwproperties_logo.jpg"
+  s.logo = File.new("#{RAILS_ROOT}/simages/jackson02.jpg")
+  
 end
